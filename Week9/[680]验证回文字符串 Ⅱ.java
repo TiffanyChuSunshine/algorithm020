d681e935fdb494e765f68b68bef0beb00f4c67fc
TiffanyChuSunshine <zhuhong_21@126.com>
2021-01-17 15:47:06 +0800

    week9

---
+++ Week9/[680]验证回文字符串 Ⅱ.java
@@ -0,0 +1,58 @@
+//给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。 
+//
+// 示例 1: 
+//
+// 
+//输入: "aba"
+//输出: True
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: "abca"
+//输出: True
+//解释: 你可以删除c字符。
+// 
+//
+// 注意: 
+//
+// 
+// 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。 
+// 
+// Related Topics 字符串 
+// 👍 309 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean validPalindrome(String s) {
+        int low = 0, high = s.length() - 1;
+        while (low < high) {
+            char c1 = s.charAt(low), c2 = s.charAt(high);
+            if (c1 == c2) {
+                low++;
+                high--;
+            } else {
+                boolean flag1 = true, flag2 = true;
+                for (int i = low, j = high - 1; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
+                        flag1 = false;
+                        break;
+                    }
+                }
+                for (int i = low + 1, j = high; i < j; i++, j--) {
+                    char c3 = s.charAt(i), c4 = s.charAt(j);
+                    if (c3 != c4) {
+                        flag2 = false;
+                        break;
+                    }
+                }
+                return flag1 || flag2;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
